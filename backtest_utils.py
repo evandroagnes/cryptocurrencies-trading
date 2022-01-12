809b5f87947c8b9aec9eb21f12f480f5bf3fd8d1
evandroagnes <evandro.agnes@gmail.com>
2022-01-12 10:32:55 +0000

    Fix the signal to use with bt.

--- backtest_utils.py
+++ backtest_utils.py
@@ -4,33 +4,38 @@ import bt
 
 def buy_and_hold_strategy(price_data, name='benchmark'):
     # Define the benchmark strategy
-    bt_strategy = bt.Strategy(name, 
-                              [bt.algos.RunOnce(),
-                               bt.algos.SelectAll(),
-                               bt.algos.WeighEqually(),
-                               bt.algos.Rebalance()])
+    s = bt.Strategy(name, 
+                    [bt.algos.RunOnce(),
+                     bt.algos.SelectAll(),
+                     bt.algos.WeighEqually(),
+                     bt.algos.Rebalance()])
     # Return the backtest
-    return bt.Backtest(bt_strategy, price_data)
+    return bt.Backtest(s, price_data)
 
 def signal_above_strategy(price_data, price_signal, name):
     # the column names must be the same
     price_data.columns = ['value']
     price_signal.columns = ['value']
 
-    bt_strategy = bt.Strategy(name, 
-                              [bt.algos.SelectWhere(price_data > price_signal),
-                               bt.algos.WeighEqually(),
-                               bt.algos.Rebalance()])
+    s = bt.Strategy(name, 
+                    [bt.algos.SelectWhere(price_data > price_signal),
+                     bt.algos.WeighEqually(),
+                     bt.algos.Rebalance()])
     # Return the backtest
-    return bt.Backtest(bt_strategy, price_data)
+    return bt.Backtest(s, price_data)
 
 def signal_strategy(price_data, signal, name):
+    weight = convert_signal_to_weight(signal.copy())
+
     # the column names must be the same
     price_data.columns = ['value']
-    signal.columns = ['value']
+    weight.columns = ['value']
 
     s = bt.Strategy(name,
-                    [bt.algos.WeighTarget(signal), 
-                    bt.algos.Rebalance()])
+                    [bt.algos.WeighTarget(weight), 
+                     bt.algos.Rebalance()])
 
     return bt.Backtest(s, price_data)
+
+def convert_signal_to_weight(signal):
+    return signal.replace(0, np.nan).replace(-1, 0).ffill().replace(np.nan, 0)
