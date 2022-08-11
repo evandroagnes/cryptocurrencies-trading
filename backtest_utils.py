8a10e79401a292b053c7b06920aa130c5c95cc87
evandroagnes <evandro.agnes@gmail.com>
2022-08-11 23:19:37 +0100

    Added parameter to change initial capital for the backtest.

--- backtest_utils.py
+++ backtest_utils.py
@@ -2,17 +2,17 @@ import numpy as np
 import pandas as pd
 import bt
 
-def buy_and_hold_strategy(price_data, name='benchmark'):
+def buy_and_hold_strategy(price_data, name='benchmark', _initial_capital=1000000.0):
     # Define the benchmark strategy
-    s = bt.Strategy(name, 
+    s = bt.Strategy(name,
                     [bt.algos.RunOnce(),
                      bt.algos.SelectAll(),
                      bt.algos.WeighEqually(),
                      bt.algos.Rebalance()])
     # Return the backtest
-    return bt.Backtest(s, price_data)
+    return bt.Backtest(strategy=s, data=price_data, integer_positions=False, initial_capital=_initial_capital)
 
-def signal_above_strategy(price_data, price_signal, name):
+def signal_above_strategy(price_data, price_signal, name, _initial_capital=1000000.0):
     # the column names must be the same
     price_data.columns = ['value']
     price_signal.columns = ['value']
@@ -22,9 +22,9 @@ def signal_above_strategy(price_data, price_signal, name):
                      bt.algos.WeighEqually(),
                      bt.algos.Rebalance()])
     # Return the backtest
-    return bt.Backtest(s, price_data)
+    return bt.Backtest(s, price_data, integer_positions=False, initial_capital=_initial_capital)
 
-def signal_strategy(price_data, signal, name):
+def signal_strategy(price_data, signal, name, _initial_capital=1000000.0):
     weight = convert_signal_to_weight(signal.copy())
 
     # the column names must be the same
@@ -35,7 +35,7 @@ def signal_strategy(price_data, signal, name):
                     [bt.algos.WeighTarget(weight), 
                      bt.algos.Rebalance()])
 
-    return bt.Backtest(s, price_data)
+    return bt.Backtest(strategy=s, data=price_data, integer_positions=False, initial_capital=_initial_capital)
 
 def convert_signal_to_weight(signal):
     return signal.replace(0, np.nan).replace(-1, 0).ffill().replace(np.nan, 0)
